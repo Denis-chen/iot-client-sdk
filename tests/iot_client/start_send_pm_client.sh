13448e7db3176b54148eaa5f2c73796f081210e9
Slav Klenov <slav.klenov@miracl.com>
2017-08-23 10:47:42 +0300

    Fixed executable location in test shell scripts. Fixed .sh files to have unix line endings.

--- tests/iot_client/start_send_pm_client.sh
+++ tests/iot_client/start_send_pm_client.sh
@@ -1,2 +1,2 @@
 cd $(dirname "$0")
-../../output/iot_client authServerUrl=https://iot.dev.miracl.net awsIoTCompliance=true identityFile=DEVICE_ID.identity mqttTlsBrokerAddr=mqtt.dev.miracl.net sendPmTo=DEVICE_ID_2
+../../output/build/default/iot_client authServerUrl=https://iot.dev.miracl.net awsIoTCompliance=true identityFile=DEVICE_ID.identity mqttTlsBrokerAddr=mqtt.dev.miracl.net sendPmTo=DEVICE_ID_2
--- tests/iot_client/start_sub_client.sh
+++ tests/iot_client/start_sub_client.sh
@@ -1,2 +1,2 @@
 cd $(dirname "$0")
-../../output/iot_client authServerUrl=https://iot.dev.miracl.net awsIoTCompliance=true identityFile=DEVICE_ID_2.identity mqttTlsBrokerAddr=mqtt.dev.miracl.net subscribeToTopic=mpin/test listenForPms=true
+../../output/build/default/iot_client authServerUrl=https://iot.dev.miracl.net awsIoTCompliance=true identityFile=DEVICE_ID_2.identity mqttTlsBrokerAddr=mqtt.dev.miracl.net subscribeToTopic=mpin/test listenForPms=true
